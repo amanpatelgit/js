7926f2c26a3e5f8394028a7fbd611f2b612ba2e4
amanpatelgit <124789525+amanpatelgit@users.noreply.github.com>
2024-07-25 13:25:48 +0000

    break and continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -28,10 +28,18 @@ for (let index = 0; index < myArray.length; index++) {
 
 // break and continue
 
-for (let index = 0; index <= 20; index++) {
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log("Detected 5");
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log("Detected 5");
-        break;
+        continue;
     }
-    console.log();
+    console.log(`value of i is ${index}`);
 }
